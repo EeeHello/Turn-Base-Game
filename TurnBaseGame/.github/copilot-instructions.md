24676d777a9957e47f5897010ee907defc872682
tyronnr1 <tyronsantospereira@gmail.com>
2025-06-24 12:45:50 +0200

    1

--- TurnBaseGame/.github/copilot-instructions.md
+++ TurnBaseGame/.github/copilot-instructions.md
@@ -2,7 +2,7 @@
 - Project name: TurnBaseGame
 - Unity version: Unity 6000.1.8f1
 - Active game object:
-  - Name: PlayerPrefabBeta
-  - Tag: Player
-  - Layer: Player
+  - Name: Basic Action Button
+  - Tag: Untagged
+  - Layer: UI
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
---
+++ TurnBaseGame/Assets/UILogicFiles/HandleButtonPress.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HandleButtonPress : MonoBehaviour
+{
+
+    public Button basicAction;
+    public Button skillAction;
+    public Button burstAction;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Onbuttonpåressdd()
+    {
+        
+    }
+}
