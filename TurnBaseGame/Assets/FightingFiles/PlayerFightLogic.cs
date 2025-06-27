8a9b6463b015e01f1a3e062053cfb2c9573fa640
tyronnr1 <tyronsantospereira@gmail.com>
2025-06-27 08:45:55 +0200

    REWORKING THE FIGHTING SYSTEM

--- TurnBaseGame/Assets/FightingFiles/PlayerFightLogic.cs
+++ TurnBaseGame/Assets/FightingFiles/PlayerFightLogic.cs
@@ -4,54 +4,21 @@ using System.Collections;
 public class PlayerFightLogic : MonoBehaviour
 {
     public Animator animator;
+
+    private void Start()
+    {
+
+    }
+
     public void PlayThrowAnimation()
     {
         animator.SetTrigger("Throw");
-        StartCoroutine(TempNotHatProjectile());
     }
 
     public void PlayKickAnimation()
     {
-        float kickLength = GetAnimationClipLength("Kick");
-        StartCoroutine(RotateKickRoutine(kickLength));
-    }
-
-    private IEnumerator RotateKickRoutine(float duration)
-    {
-        transform.Rotate(0, -90, 0);
-
         animator.SetTrigger("Kick");
-        Debug.Log("Player Kick Animation");
-
-        yield return new WaitForSeconds(duration);
-
-        transform.Rotate(0, 90, 0);
-    }
-
-    private float GetAnimationClipLength(string clipName)
-    {
-        RuntimeAnimatorController ac = animator.runtimeAnimatorController;
-        foreach (var clip in ac.animationClips)
-        {
-            if (clip.name == clipName)
-            {
-                return clip.length;
-            }
-        }
-        Debug.LogWarning($"Animation clip '{clipName}' not found!");
-        return 1f; // fallback duration
-    }
-
-    public IEnumerator TempNotHatProjectile()
-    {
-        yield return new WaitForSeconds(0.8f);
-        GameObject projectile = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        projectile.transform.parent = transform;
-        projectile.transform.localScale = new Vector3(0.5f, 0.25f,0.5f);
-        projectile.transform.position = transform.position + new Vector3(1, 0.800000012f, 0);
-
-        projectile.AddComponent<HatProjectile>();
-        projectile.GetComponent<HatProjectile>().BasicAction(FindEnemyInSlot(1) + new Vector3(0,1.8f,0));
+       
 
     }
 
